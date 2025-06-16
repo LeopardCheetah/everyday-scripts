755e77df70bcbb2eeb0332f627d58beb1c8a9d85
LeopardCheetah <hawkswithmath@gmail.com>
2025-06-16 11:22:58 -0700

    finished time tracker tracker

---
+++ time_tracker_2/readme.md
@@ -0,0 +1,4 @@
+based off of time tracker 1    
+hopefully this one is better
+
+note: if you want to run this locally, you must have a folder called `day_recaps` made in the same directory as this script
---
+++ time_tracker_2/time_tracker_scr.py
@@ -0,0 +1,32 @@
+from datetime import date
+import os
+
+import tt
+
+
+############################
+# see the script file to actually change prompt defaults and such
+############################
+
+
+
+#####
+# get the day recap
+to_write = tt.time_tracker_script()
+######
+
+
+day = date.today().isoformat()
+
+path = f'day_recaps/{day}.txt' # change this to wherever you want to store files i guess
+
+if os.path.isfile(path):
+    # file exists, delete it
+    os.remove(path)
+
+with open(path, "w") as f:
+    f.write(f'{day}\n')
+    for _ls in to_write:
+        f.write('--------------------------------------' + '\n')
+        for _l in _ls:
+            f.write(_l + '\n')
