f12254c2cea0ab8d6c032a61cbd6d6b78dc08f50
LeopardCheetah <hawkswithmath@gmail.com>
2025-08-10 10:11:12 -0700

    rewrite tt

--- time-tracker-2/time_tracker_scr.py
+++ time-tracker-2/time_tracker_scr.py
@@ -1,50 +1,43 @@
+
+################################
+
+# Defaults that can be changed #
+
+
+# makes it so that if its 2:43am 
+# and i do a day recap, the date 
+# of the day is still from the 
+# previous day
+hour_offset = 2                
+
+
+################################
+
+
+#############################################
+# script -- don't change anything below pls #
+#############################################
+
+
 import time 
 import os
 
 # for those pulling from code base use this import
 # import tt
-import devdev as tt
+import tscr as tt
 
-############################
-# see the script file to actually change prompt defaults and such
-############################
 
-# makes it so that if its 2:43am and i do a day recap, the date of the day is still from the previous day
-hour_offset = 2
-
-#####
 # get the day recap
 to_write = tt.time_tracker_script()
-######
+
+# format: YYYY-MM-DD
+d = time.strftime("%Y-%m-%d", time.localtime(time.time() - 60*60*hour_offset))
+
+# format: YYYY-MM-DD HH:MM:SS (24h time)
+t_formatted = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
 
-
-t = time.localtime(time.time() - 60*60*hour_offset)
-
-d = str(t.tm_year) + '-'
-if t.tm_mon < 10:
-    d += '0'
-d += str(t.tm_mon) + '-'
-if t.tm_mday < 10:
-    d += '0'
-d += str(t.tm_mday)
-
-
-
-t_written = time.localtime(time.time())
-
-dx = str(t_written.tm_year) + '-'
-if t_written.tm_mon < 10:
-    dx += '0'
-dx += str(t_written.tm_mon) + '-'
-if t_written.tm_mday < 10:
-    dx += '0'
-dx += str(t_written.tm_mday)
-
-t_formatted = time.strftime(dx+" %H:%M:%S", time.localtime())
-
-
-path = f'day_recaps/{d}.txt' # change this to wherever you want to store files i guess
+path = f'day_recaps/{d}.txt' 
 
 if os.path.isfile(path):
     # file exists, delete it
